016047d1e9f0d16c354bf47850bb00cf8916363a
christopherayork <christopherayork1992@gmail.com>
2020-12-23 22:42:29 -0500

    Added the courses and course components, plus a mockup request service

---
+++ src/app/courses.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import {CoursesService} from './courses.service';
+
+@Component({
+  selector: 'courses',
+  template: `
+    <h2>{{ title }}</h2>
+    <h3>{{ courses.length + ' Authors' }}</h3>
+    <ul>
+      <li *ngFor="let course of courses">
+        {{ course }}
+      </li>
+    </ul>
+  `
+})
+export class CoursesComponent {
+  title = 'List of courses';
+  courses;
+
+
+  constructor(service: CoursesService) {
+    this.courses = service.getCourses();
+  }
+
+}
